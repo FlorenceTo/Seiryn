9f3ab1791778dc380ef55b492c9dadb0789c6d1c
FlorenceTo <Florenceto@live.co.uk>
2025-08-13 23:41:22 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,94 +1,78 @@
 let shapes = [];
-let audioUnlocked = false;
-let keyMap = {};
-let scaleNotes = [130.81, 138.59, 164.81, 185.00, 207.65, 233.08, 246.94, 261.63];
-let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+let keyMap = {
+  'A': { type: 'sphere', freq: 600, color: [255, 0, 0] },
+  'S': { type: 'box', freq: 300, color: [0, 255, 0] },
+  'D': { type: 'sphere', freq: 700, color: [255, 255, 255] },
+};
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   noStroke();
-  textAlign(CENTER, CENTER);
-  textSize(24);
-
-  // Map letters to shapes and colors
-  for (let i = 0; i < letters.length; i++) {
-    keyMap[letters[i]] = {
-      type: i % 2 === 0 ? 'circle' : 'square',
-      baseFreq: scaleNotes[i % scaleNotes.length],
-      color: [random(50,255), random(50,255), random(50,255)]
-    };
-  }
 }
 
 function draw() {
-  background(0, 20);
+  background(0);
 
-  if (!audioUnlocked) {
-    fill(255);
-    text('Click to start', width/2, height/2);
-    return;
-  }
-
-  // Draw and update shapes
   for (let i = shapes.length - 1; i >= 0; i--) {
     let s = shapes[i];
+
+    // Move shapes
     s.x += s.vx;
     s.y += s.vy;
+    s.z += s.vz;
 
-    // Draw trail
-    for (let t of s.trail) {
-      fill(s.color[0], s.color[1], s.color[2], t.opacity);
-      if (s.type === 'circle') ellipse(t.x, t.y, 20);
-      else rect(t.x-10, t.y-10, 20, 20);
-      t.opacity *= 0.9;
-    }
-
-    // Draw main shape
+    push();
+    translate(s.x, s.y, s.z);
     fill(s.color[0], s.color[1], s.color[2], s.opacity);
-    if (s.type === 'circle') ellipse(s.x, s.y, 30);
-    else rect(s.x-15, s.y-15, 30, 30);
+    let size = map(s.baseFreq, 200, 900, 50, 150);
+    if (s.type === 'sphere') sphere(size);
+    else box(size);
+    pop();
 
-    // Update opacity
-    s.opacity *= 0.97;
-    if (s.opacity < 1) {
-      s.osc.amp(0, 1);
-      s.osc.stop(1);
-      shapes.splice(i,1);
+    s.opacity -= 1;
+    if (s.opacity <= 0) {
+      s.osc.amp(0, 0.2);
+      s.osc.stop(0.2);
+      shapes.splice(i, 1);
     }
-  }
-}
-
-function mousePressed() {
-  if (!audioUnlocked) {
-    userStartAudio();
-    audioUnlocked = true;
   }
 }
 
 function keyPressed() {
-  if (!audioUnlocked) return;
+  userStartAudio(); // important for sound in browser
+
   let k = key.toUpperCase();
   if (!keyMap[k]) return;
 
-  let cfg = keyMap[k];
-  let osc = new p5.Oscillator('sine');
-  osc.freq(cfg.baseFreq);
+  let config = keyMap[k];
+  let osc = config.type === 'sphere' ? new p5.Oscillator('triangle') : new p5.Oscillator('sine');
+  osc.freq(config.freq);
   osc.start();
-  osc.amp(0.5, 0.1);
+  osc.amp(0.5, 0.05);
 
   let s = {
-    x: random(width),
-    y: random(height),
-    vx: random(-2,2),
-    vy: random(-2,2),
-    type: cfg.type,
-    color: cfg.color,
-    opacity: 255,
-    trail: [{x: random(width), y: random(height), opacity:255}],
-    osc: osc
+    x: random(-width/2, width/2),
+    y: random(-height/2, height/2),
+    z: random(-500, 500),
+    vx: random(-1, 1),
+    vy: random(-1, 1),
+    vz: random(-1, 1),
+    type: config.type,
+    baseFreq: config.freq,
+    osc: osc,
+    color: config.color,
+    opacity: 255
   };
-
   shapes.push(s);
+}
+
+function keyReleased() {
+  if (shapes.length > 0) {
+    let s = shapes[shapes.length - 1];
+    s.osc.amp(0, 0.5);
+    s.osc.stop(0.5);
+  }
 }
 
 function windowResized() {
