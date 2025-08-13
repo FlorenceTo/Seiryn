3e6d407c3da24202c8ff38adc8afe01b36e7ffc7
FlorenceTo <Florenceto@live.co.uk>
2025-08-14 00:38:57 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 function getFreqRandomOctave(interval) {
-  // Pick octave -2 or -1 relative to C4
-  let octaveShift = random([-2, -1]);
-  return baseNote * pow(2, octaveShift) * pow(2, interval / 12);
+  // Only use the lower octave (C3-ish)
+  let baseFreq = 130.81; // C3
+  return baseFreq * pow(2, interval / 12) * random(0.98, 1.02); // tiny detune
 }
 
 function keyPressed() {
@@ -14,25 +14,25 @@ function keyPressed() {
   let freq = getFreqRandomOctave(interval);
 
   let osc = new p5.Oscillator('triangle');
-  osc.freq(freq * random(0.98, 1.02)); // slight detune
+  osc.freq(freq);
   osc.start();
 
   let filter = new p5.LowPass();
-  filter.freq(1500);
-  filter.res(2);
+  filter.freq(1200); // slightly lower to smoothen
+  filter.res(1.5);
   osc.disconnect();
   osc.connect(filter);
 
   let env = new p5.Envelope();
-  env.setADSR(0.05, 0.1, 0.3, 0.7);
-  env.setRange(0.4, 0);
+  env.setADSR(0.05, 0.1, 0.3, 0.6);
+  env.setRange(0.35, 0);
   env.play(osc);
 
   let reverb = new p5.Reverb();
-  reverb.process(osc, 2, 2); // reverb stays dreamy
+  reverb.process(osc, 2, 2); 
 
   let delay = new p5.Delay();
-  delay.process(osc, 0.075, 0.3, 1200); // delay time halved
+  delay.process(osc, 0.075, 0.25, 1000); // shorter and smoother
 
   let amp = new p5.Amplitude();
   amp.setInput(osc);
